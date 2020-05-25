c5defcd1f8c2ded6f7c9681d84299c7dfc16086f
ghblog-git <1732127846@qq.com>
2020-05-26 07:09:07 +0800

    Site updated: 2020-05-26 07:09:07

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/joke.ico");
-         document.title = '！！这里这里 ◕ ں ◕ ';
+         document.title = 'Σ(っ °Д °;)っ喔哟，崩溃啦！ ';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '(ฅ>ω<*ฅ) 喵喵φ(゜▽゜*)♪爱你哟~' + OriginTitle;
+         document.title = 'φ(゜▽゜*)♪咦，又好了！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
